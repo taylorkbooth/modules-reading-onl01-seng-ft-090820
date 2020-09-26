1bf1974a0adad8238ad61b7d10a70677b3a99e28
Taylor Booth <taylorkbooth@gmail.com>
2020-09-26 19:24:39 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,6 +3,7 @@ require_relative './class_methods_module.rb'
 
 class Kid
   include Dance
+  extend MetaDancing
  
   attr_accessor :name
  
@@ -10,9 +11,5 @@ class Kid
     @name = name
   end
   
-  class Dancer
-  extend MetaDancing
-end 
-
 
 end
